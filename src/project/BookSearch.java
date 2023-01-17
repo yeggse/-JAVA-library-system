e395cd2a7bda2287697e4b54948ad103f572cce9
tj-bu-03 <tj-bu-03@DESKTOP-ENSET2K>
2023-01-17 14:34:12 +0900

    6. 1/17 오후 커밋 1차

--- src/project/BookSearch.java
+++ src/project/BookSearch.java
@@ -15,6 +15,7 @@ public class BookSearch extends JFrame{
 	Statement stmt3 = null;
 	static String id;
 	int count;
+	
 	// JTable
 	Object ob[][] = new Object[0][5]; //데이터 표시에 열만 나오게 설정
 	DefaultTableModel model;  // 데이터 저장 부분
@@ -26,6 +27,8 @@ public class BookSearch extends JFrame{
 	Connection con = null;
 	PreparedStatement pstmt = null;	//sql구문 실행
 	ResultSet rs = null; // select구문 사용시 필수
+	
+//	Object data[] = null;
 	
 	BookSearch(String id){
 		this.id = id;
@@ -69,9 +72,11 @@ public class BookSearch extends JFrame{
 		// sql 연결
 		try {
 			Class.forName("com.mysql.jdbc.Driver"); // MySQL 드라이버 로드
+			con = DriverManager.getConnection
+					("jdbc:mysql://localhost:3306/psersonalprojet", "root","test123"); // JDBC 연결
 			conn = DriverManager.getConnection
 					("jdbc:mysql://localhost:3306/psersonalprojet", "root","test123"); // JDBC 연결
-			System.out.println("좌석 : DB 연결 완료");
+			System.out.println("도서 검색 : DB 연결 완료");
 			stmt = conn.createStatement();	// SQL문 처리용 Statement 객체 생성
 			stmt1 = conn.createStatement();	// SQL문 처리용 Statement 객체 생성
 			stmt2 = conn.createStatement();	// SQL문 처리용 Statement 객체 생성
@@ -88,15 +93,23 @@ public class BookSearch extends JFrame{
 		searching.setSize(300,30);
 		c.add(searching);
 		
-		// 검색 버튼
-		// tf를 읽어서 DB에서 검색되도록 이벤트 설정 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		JButton searchBtn = new JButton("검색");
-		searchBtn.setLocation(410,169);
-		searchBtn.setSize(70,30);
-		searchBtn.setBackground(new Color(72,61,139));
-		searchBtn.setFont(new Font("함초롱돋움",Font.BOLD, 15));
-		searchBtn.setForeground(Color.white);
-		c.add(searchBtn);
+		// 리셋 버튼
+		JButton resetBtn = new JButton("re");
+		resetBtn.setLocation(490,169);
+		resetBtn.setSize(50,30);
+		resetBtn.setBackground(new Color(72,61,139));
+		resetBtn.setFont(new Font("함초롱돋움",Font.BOLD, 15));
+		resetBtn.setForeground(Color.white);
+		c.add(resetBtn);
+		
+		resetBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 		
 		// 결과 도출 패널
 		//DB에서 도서 갯수 찾은 다음 count 로 도출하기!!!!!!!!!!!!!!!!!
@@ -107,45 +120,66 @@ public class BookSearch extends JFrame{
 		resultPanel.setLayout(null);
 		c.add(resultPanel);
 		
+		// 결과 도출 그래프
+		// DB에서 검색되도록!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		model = new DefaultTableModel(ob,str);	// 데이터 저장[][], 컬럼 명
+		table = new JTable(model);
+		js = new JScrollPane(table);
+		js.setLocation(20,50);
+		js.setSize(480,180);
+		resultPanel.add(js);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		
+		// 검색 버튼
+		// tf를 읽어서 DB에서 검색되도록 이벤트 설정 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		JButton searchBtn = new JButton("검색");
+		searchBtn.setLocation(410,169);
+		searchBtn.setSize(70,30);
+		searchBtn.setBackground(new Color(72,61,139));
+		searchBtn.setFont(new Font("함초롱돋움",Font.BOLD, 15));
+		searchBtn.setForeground(Color.white);
+		
+		searchBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				try {
+					ResultSet srs = stmt3.executeQuery("select * from book where book_title like '%"+searching.getText()+"%';");
+					System.out.println("select * from book where book_title like '%"+searching.getText()+"%';");
+					if(srs.next()) {
+						//출력하기
+						String tit = srs.getString("book_title");
+						String publ = srs.getString("book_publisher");
+						String auth = srs.getString("book_author");
+						String loca = srs.getString("book_location");
+						
+						Object data[] = {tit, publ, auth, loca};
+						model.addRow(data);
+						String sql = "select * from book where book_title = '"+searching.getText()+"';";
+						
+						//count 갯수 설정
+						count = srs.getInt(1);
+					} else {
+						JOptionPane.showMessageDialog(null, "보유 중인 도서가 존재하지 않습니다.", "책 없음", JOptionPane.ERROR_MESSAGE);
+					}
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					System.out.println("도서 검색 버튼 에러");
+				}
+			}
+		});
+		c.add(searchBtn);
+		
+		//DB에서 도서 갯수 찾은 다음 count 로 도출하기!!!!!!!!!!!!!!!!!
 		JLabel resultintro = new JLabel("찾으시는 도서는 "+count+"권 보유 중입니다.");
 		resultintro.setLocation(150,5);
 		resultintro.setSize(300,30);
 		resultintro.setFont(new Font("함초롱돋움",Font.BOLD, 15));
 		resultPanel.add(resultintro);
-		
-		// 결과 도출 그래프
-		// DB에서 검색되도록!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		//Jtable 가운데 배치
-		model = new DefaultTableModel(ob,str);	// 데이터 저장[][], 컬럼 명
-		table = new JTable(model);
-		js = new JScrollPane(table);
-		table.setLocation(20,50);
-		table.setSize(480,180);
-		resultPanel.add("Center",js);
-		resultPanel.add(table);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		
-		
-		// DB출력
-		String sql = "select * from book";
-		try {
-			pstmt=con.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				String tit = rs.getString("book_title");
-				String publ = rs.getString("book_publisher");
-				String auth = rs.getString("book_author");
-				String loca = rs.getString("book_location");
-				
-				Object data[] = {tit, publ, auth, loca};
-				model.addRow(data);
-			}
-		} catch (SQLException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
+
 		
 		// 뒤로 가기
 		BackBTN back = new BackBTN(this);
--- src/projectDialogs/ss.java
+++ src/projectDialogs/ss.java
@@ -3,16 +3,20 @@ package projectDialogs;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-public class ss extends JFrame implements ActionListener{
+import project.BookSearch;
+
+public class ss extends JFrame {
 	// JTable
 	Object ob[][] = new Object[0][5]; //데이터 표시에 열만 나오게 설정
 	DefaultTableModel model;  // 데이터 저장 부분
@@ -25,6 +29,14 @@ public class ss extends JFrame implements ActionListener{
 	PreparedStatement pstmt = null;	//sql구문 실행
 	ResultSet rs = null; // select구문 사용시 필수
 	
+	Connection conn;
+	Statement stmt = null;
+	Statement stmt1 = null;
+	Statement stmt2 = null;
+	Statement stmt3 = null;
+	static String id;
+	int count;
+	
 	ss(){
 		super("타이틀 입력");
 		
@@ -32,12 +44,27 @@ public class ss extends JFrame implements ActionListener{
 		model = new DefaultTableModel(ob,str);	// 데이터 저장[][], 컬럼 명
 		table = new JTable(model);
 		js = new JScrollPane(table);
-		this.add("Center",js);
+		add(js);
 		setBounds(250,250,300,300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		
 		// DB연결
+		// sql 연결
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); // MySQL 드라이버 로드
+			conn = DriverManager.getConnection
+					("jdbc:mysql://localhost:3306/psersonalprojet", "root","test123"); // JDBC 연결
+			System.out.println("좌석 : DB 연결 완료");
+			stmt = conn.createStatement();	// SQL문 처리용 Statement 객체 생성
+			stmt1 = conn.createStatement();	// SQL문 처리용 Statement 객체 생성
+			stmt2 = conn.createStatement();	// SQL문 처리용 Statement 객체 생성
+			stmt3 = conn.createStatement();	// SQL문 처리용 Statement 객체 생성
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBC 드라이버 로드 에러");
+		} catch (SQLException e) {
+			System.out.println("DB 연결 오류");
+		}
 		
 		// DB출력
 		String sql = "select * from book";
@@ -67,14 +94,12 @@ public class ss extends JFrame implements ActionListener{
 	
 	
 	
-	
-	
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		new ss();
 	}
+	
+	
+
 	
 }
