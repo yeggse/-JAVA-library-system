068f41faddd0956461e9c84546173b585998d4cc
tj-bu-03 <tj-bu-03@DESKTOP-ENSET2K>
2023-01-18 17:59:06 +0900

    10. 1/18 최종 커밋. 관리자페이지 생성 + DB업로드

---
+++ people.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM psersonalprojet.people
+LIMIT 0, 1000
+
+-- Date: 2023-01-18 17:57
+*/
+INSERT INTO `` (`id`,`pw`,`name`,`gender`,`address`,`royal`,`birth`) VALUES ('test1','123','강호동','남','서울','O','670405');
+INSERT INTO `` (`id`,`pw`,`name`,`gender`,`address`,`royal`,`birth`) VALUES ('test2','456','유재석','남','대전','X','710924');
+INSERT INTO `` (`id`,`pw`,`name`,`gender`,`address`,`royal`,`birth`) VALUES ('test3','789','이경규','남','대구','O','541216');
+INSERT INTO `` (`id`,`pw`,`name`,`gender`,`address`,`royal`,`birth`) VALUES ('test4','1234','김숙','여','부산','X','760222');
+INSERT INTO `` (`id`,`pw`,`name`,`gender`,`address`,`royal`,`birth`) VALUES ('test5','4567','송은이','여','광주','O','711030');
+INSERT INTO `` (`id`,`pw`,`name`,`gender`,`address`,`royal`,`birth`) VALUES ('test6','123','전소민','여','당진','X','860710');
+INSERT INTO `` (`id`,`pw`,`name`,`gender`,`address`,`royal`,`birth`) VALUES ('test7','123','하동균','남','제주','O','810925');
+INSERT INTO `` (`id`,`pw`,`name`,`gender`,`address`,`royal`,`birth`) VALUES ('test8','123','이나영','남','안양','X','781128');
+INSERT INTO `` (`id`,`pw`,`name`,`gender`,`address`,`royal`,`birth`) VALUES ('test9','456','육종완','남','충주','X','770416');
---
+++ seat.sql
@@ -0,0 +1,30 @@
+/*
+-- Query: SELECT * FROM psersonalprojet.seat
+LIMIT 0, 1000
+
+-- Date: 2023-01-18 17:58
+*/
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (1,'O','test1','강호동');
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (2,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (3,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (4,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (5,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (6,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (7,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (8,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (9,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (10,'O','test9',NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (11,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (12,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (13,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (14,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (15,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (16,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (17,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (18,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (19,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (20,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (21,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (22,'X',NULL,NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (23,'O','test8',NULL);
+INSERT INTO `` (`seat_no`,`seat_lent`,`id`,`name`) VALUES (24,'X',NULL,NULL);
