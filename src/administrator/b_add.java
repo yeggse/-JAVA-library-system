b8f6d636b62200372d8f00b94b3b3c38d5b6d6f8
tj-bu-03 <tj-bu-03@DESKTOP-ENSET2K>
2023-01-19 10:11:33 +0900

    12. 1/19

--- src/administrator/b_add.java
+++ src/administrator/b_add.java
@@ -7,22 +7,25 @@ import java.awt.Font;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class b_add extends JDialog{
 	b_add(JFrame f){
 		project.Intro s = (project.Intro)f; 
 		setSize(300,400);
 		this.setResizable(false);
-		setTitle("비밀번호 찾기 시스템");
+		setTitle("(관리자) 도서 추가 시스템");
 		Container c = getContentPane();
 		c.setLayout(null);
 		c.setBackground(new Color(224,224,224));
 		
-		JLabel title = new JLabel("비밀번호 찾기");
+		JLabel title = new JLabel("도서 추가 시스템");
 		title.setSize(200,30);
 		title.setLocation(65,20);
 		title.setFont(new Font("휴먼둥근헤드라인", Font.BOLD, 23));
 		c.add(title);
 		
+		JPanel panel = new JPanel();
+		panel.setSize(250,300);
 	}
 }
